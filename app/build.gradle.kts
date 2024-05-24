823eac5837f2f9620a7ffbb0f6807ae2a6a3d8e3
pipe0220 <pipedorado14@gmail.com>
2024-05-24 17:44:49 -0500

    creacion de funciones colores botones y pantalla registro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.material:material:1.13.0-alpha02")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
